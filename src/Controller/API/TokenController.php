b035fe0476348c892265d0c9cb1ea61135123633
petrero <petrero@hotmail.com>
2018-10-08 15:17:22 -0500

    Head back to TokenController. Here's the process:
    1.Check that the username and password are correct.
    2.Generate a JSON web token.
    3.Send it back to the client.

--- src/Controller/API/TokenController.php
+++ src/Controller/API/TokenController.php
@@ -2,14 +2,42 @@
 namespace App\Controller\API;
 
 use App\Controller\APIBaseController;
+use App\Repository\UserRepository;
+use Lexik\Bundle\JWTAuthenticationBundle\Encoder\JWTEncoderInterface;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use Symfony\Component\Security\Core\Exception\BadCredentialsException;
 
 class TokenController extends APIBaseController {
 	/**
 	 * @Route("/api/tokens", methods="POST")
 	 */
-	public function newTokenAction(){
-		return new Response('Token!');
+	public function newTokenAction(
+		Request $request,
+		UserRepository $userRepository,
+		UserPasswordEncoderInterface $userPasswordEncoder,
+		JWTEncoderInterface $lexikEncoder
+	){
+		$user = $userRepository->findOneBy(['username' => $request->getUser()]);
+		if(!$user){
+			$this->createNotFoundException('No user');
+		}
+
+		$isValid = $userPasswordEncoder
+					->isPasswordValid($user, $request->getPassword());
+
+		if(!$isValid){
+			throw new BadCredentialsException();
+		}
+
+		$token = $lexikEncoder
+			->encode(['username' => $user->getUsername()]);
+
+		return new JsonResponse([
+			'token' => $token
+		]);
 	}
 }
